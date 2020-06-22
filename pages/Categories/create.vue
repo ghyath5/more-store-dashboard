ff0d99bd36290ee42a8cabaa709ee3163ab2004d
ghyath <ghyathdarwish@gmail.com>
2020-06-22 23:49:58 +0300

    Create category page

---
+++ pages/Categories/create.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>Create Categories</div>
+</template>
+<script>
+export default {
+	middleware({ store, redirect }) {
+		if (!store.state.permissions.includes('create_categories')) {
+			return redirect('/');
+		}
+	},
+};
+</script>
