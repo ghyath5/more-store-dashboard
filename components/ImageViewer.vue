c426efa92b19758febb88df507518a4a2c1e76be
ghyath <ghyathdarwish@gmail.com>
2020-06-22 14:57:08 +0300

    Image viewrer component

---
+++ components/ImageViewer.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<v-img :max-width="maxWidth" :max-height="maxHeight" :aspect-ratio="aspectRatio" :src="imageSrc" />
+	</div>
+</template>
+<script>
+export default {
+	name: 'ImageViewer',
+	props: {
+		value: {
+			type: Object,
+			default() {
+				return null;
+			},
+		},
+		aspectRatio: {
+			type: Number,
+			default() {
+				return 1.8;
+			},
+		},
+		maxWidth: {
+			type: Number,
+			default() {
+				return 80;
+			},
+		},
+		maxHeight: {
+			type: Number,
+			default() {
+				return 50;
+			},
+		},
+	},
+	computed: {
+		imageSrc() {
+			return this.value && this.value.objectId ? this.value.objectId : '/no_image.png';
+		},
+	},
+};
+</script>
