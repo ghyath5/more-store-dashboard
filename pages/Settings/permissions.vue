c3377c20c3d4b89bf0c28e82f13a811de66cb5ba
ghyath <ghyathdarwish@gmail.com>
2020-06-23 22:40:06 +0300

    Fix add and remove permissions

--- pages/Settings/permissions.vue
+++ pages/Settings/permissions.vue
@@ -1,12 +1,16 @@
 <template>
 	<v-container>
-		<v-btn @click="associate" :disabled="!changed" class="mb-2" small color="success">Save changes</v-btn>
 		<v-data-table
 			fixed-header
 			:loading="Boolean(loading)"
 			:headers="headers"
 			:items="permissions"
+			:server-items-length="permissionsCount"
+			:items-per-page.sync="queryVariables.itemPerPage"
+			:page.sync="queryVariables.page"
+			ref="permissions-table"
 			group-by="permission_group_name"
+			sort-by="name"
 			height="410"
 		>
 			<template v-slot:group.header="{ group }">
@@ -23,6 +27,8 @@
 						</template>
 						<template v-else>
 							<v-checkbox
+								:readonly="column.value === 'admin'"
+								:color="column.value === 'admin' ? 'success' : ''"
 								@change="setRolePermission(item.name, column.value, $event)"
 								:input-value="column.permissions.includes(item.name)"
 								class="mx-2"
@@ -35,62 +41,84 @@
 	</v-container>
 </template>
 <script>
-import rolesGql from '~/graphql/roles/all.gql';
-import associatePermissionsGql from '~/graphql/roles/associatePermissionsWithRole.gql';
-import permissionGql from '~/graphql/permissions.gql';
+import rolesGql from '~/gql/roles/all.gql';
+import associatePermissionsGql from '~/gql/roles/associatePermissionsWithRole.gql';
+import permissionGql from '~/gql/roles/permissions.gql';
+import deletePermissionGql from '~/gql/roles/delete_roles_permissions.gql';
 export default {
-	middleware({ store, redirect }) {
-		if (!store.state.permissions.includes('manage_roles')) {
+	middleware({ store, redirect, app }) {
+		if (!app.$has_permission('manage_roles')) {
 			return redirect('/');
 		}
 	},
 	methods: {
-		associate() {
-			let roles = this.headers.filter(header => header.id !== 1);
-			let objects = [];
-			let permissions = [];
-			roles.map(role => {
-				permissions.push(...role.permissions);
-				role.permissions.map(permission => {
-					objects.push({
-						permission,
-						role_name: role.value,
+		getPermissions() {
+			let variables = {
+				limit: this.queryVariables.itemPerPage,
+				offset: (this.queryVariables.page - 1) * this.queryVariables.itemPerPage,
+			};
+			this.$apollo.queries.allPermissions.refetch(variables);
+		},
+		setRolePermission(permission, role_name, checked) {
+			if (checked) {
+				this.$apollo.mutate({
+					mutation: associatePermissionsGql,
+					variables: {
+						objects: {
+							permission,
+							role_name,
+						},
+						on_conflict: {
+							constraint: 'roles_permissions_role_name_permission_key',
+							update_columns: ['permission', 'role_name'],
+						},
+					},
+				});
+				return;
+			}
+			this.$apollo
+				.mutate({
+					mutation: deletePermissionGql,
+					variables: {
+						where: {
+							_and: [{ permission: { _eq: permission } }, { role_name: { _eq: role_name } }],
+						},
+					},
+				})
+				.catch(e => {
+					console.log(e);
+
+					this.$store.commit('setSnack', {
+						active: true,
+						color: 'error',
 					});
 				});
-			});
-			// console.log(permissions);
-
-			this.$apollo.mutate({
-				mutation: associatePermissionsGql,
-				variables: {
-					objects,
-					on_conflict: {
-						constraint: 'roles_permissions_role_name_permission_key',
-						update_columns: ['permission', 'role_name'],
-					},
-				},
-			});
 		},
-		setRolePermission(permission, column, checked) {
-			let role = this.headers.find(col => col.value === column);
-			let permissions = role.permissions;
-			if (checked) {
-				permissions.push(permission);
-			} else {
-				permissions = permissions.filter(p => p !== permission);
-			}
-			role.permissions = permissions;
-			console.log(role, role.permissions);
+	},
+	watch: {
+		queryVariables: {
+			handler() {
+				this.getPermissions();
+				let table = this.$refs['permissions-table'];
+				let wrapper = table.$el.querySelector('div.v-data-table__wrapper');
 
-			this.changed = true;
+				this.$vuetify.goTo(table); // to table
+				this.$vuetify.goTo(table, { container: wrapper }); // to header
+			},
+			deep: true,
 		},
 	},
 	data() {
 		return {
+			queryVariables: {
+				itemPerPage: 10,
+				page: 1,
+			},
 			changed: false,
 			loading: 0,
 			headers: [],
 			permissions: [],
+			permissionsCount: 0,
 			roles_permissions: {},
 		};
 	},
@@ -135,17 +163,18 @@ export default {
 		},
 		allPermissions: {
 			query: permissionGql,
-			// variables() {
-			// 	return {
-			// 		limit: 10,
-			// 		offset: 0,
-			// 	};
-			// },
+			variables() {
+				return {
+					limit: 10,
+					offset: 0,
+				};
+			},
 			update(data) {
 				return data;
 			},
 			result({ data, loading, networkStatus }) {
 				this.permissions = data.permissions;
+				this.permissionsCount = data.permissions_aggregate.aggregate.count;
 			},
 			error(error) {
 				console.error("We've got an error!", error);
