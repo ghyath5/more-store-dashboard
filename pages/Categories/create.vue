a817d51c281338d8b8c98a39c78bb0ad62b1997b
ghyath <ghyathdarwish@gmail.com>
2020-06-25 00:34:11 +0300

    Create category page

--- pages/Categories/create.vue
+++ pages/Categories/create.vue
@@ -1,8 +1,20 @@
 <template>
-	<div>Create Categories</div>
+	<v-container>
+		<item-editor :headers="$store.state.categoryHeaders.filter(e => !e.notEditable)"></item-editor>
+	</v-container>
 </template>
 <script>
+import ItemEditor from '~/components/ItemEditor';
+import categoriesGql from '~/gql/categories/all.gql';
 export default {
+	components: {
+		ItemEditor,
+	},
+	data() {
+		return {
+			categoriesGql,
+		};
+	},
 	middleware({ store, redirect }) {
 		if (!store.state.permissions.includes('create_categories')) {
 			return redirect('/');
