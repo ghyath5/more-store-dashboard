e6bf24bba813eb4d420589a0820c597aecef02fb
ghyath <ghyathdarwish@gmail.com>
2020-06-25 23:39:57 +0300

    Pass createGql to editor

--- pages/Categories/create.vue
+++ pages/Categories/create.vue
@@ -1,17 +1,23 @@
 <template>
 	<v-container>
-		<item-editor :headers="$store.state.categoryHeaders.filter(e => !e.notEditable)"></item-editor>
+		<item-editor
+			:createGql="createGql"
+			:headers="$store.state.categoryHeaders.filter(e => !e.notEditable)"
+			@itemCreated="created"
+		></item-editor>
 	</v-container>
 </template>
 <script>
 import ItemEditor from '~/components/ItemEditor';
 import categoriesGql from '~/gql/categories/all.gql';
+import createGql from '~/gql/categories/create.gql';
 export default {
 	components: {
 		ItemEditor,
 	},
 	data() {
 		return {
+			createGql,
 			categoriesGql,
 		};
 	},
@@ -19,6 +25,16 @@ export default {
 		if (!store.state.permissions.includes('create_categories')) {
 			return redirect('/');
 		}
+	},
+	methods: {
+		created(data) {
+			this.$store.commit('setSnack', {
+				active: true,
+				color: 'success',
+				text: 'Category created successfully!',
+			});
+			this.$router.push('/categories');
+		},
 	},
 };
 </script>
