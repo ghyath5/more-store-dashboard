d5b541524be79f0dc78c432a2dc28c8ab178b358
ghyath <ghyathdarwish@gmail.com>
2020-06-26 23:46:32 +0300

    userHeaders, roleHeaders

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import categoriesGql from '~/gql/categories/all.gql';
+import permissionsGql from '~/gql/roles/permissions.gql';
 export const state = () => ({
 	categoryHeaders: [
 		{
@@ -70,6 +71,115 @@ export const state = () => ({
 			text: '',
 			viewer: 'icon',
 			width: 30,
+			sortable: false,
+			notEditable: true,
+		},
+	],
+	userHeaders: [
+		{
+			id: 100,
+			text: 'Name',
+			value: 'name',
+			viewer: 'text',
+			editor: 'textEditor',
+			rules: [v => !!v || 'Name is required'],
+			settings: {
+				// fieldWidth:70 //in %
+			},
+		},
+		{
+			id: 200,
+			text: 'Email',
+			value: 'email',
+			viewer: 'text',
+			editor: 'textEditor',
+			rules: [v => !!v || 'Email is required'],
+			settings: {
+				// fieldWidth:70 //in %
+			},
+		},
+		{
+			id: 300,
+			text: 'Roles',
+			value: 'roles',
+			viewer: 'text',
+			editor: 'autocomplete',
+			sortable: false,
+			settings: {
+				queryGql: categoriesGql,
+				itemValue: 'id',
+				itemText: 'name',
+				searchModel: 'categories',
+				searchOptions: {
+					key: 'name',
+					op: '_ilike',
+				},
+			},
+		},
+		{
+			id: 800,
+			text: 'Updated At',
+			value: 'updated_at',
+			viewer: 'date',
+			width: 180, //in px
+			notEditable: true,
+		},
+		{
+			id: 1000,
+			text: '#',
+			viewer: 'actions',
+			width: 50,
+			sortable: false,
+			notEditable: true,
+		},
+	],
+	roleHeaders: [
+		{
+			id: 100,
+			text: 'Name',
+			value: 'name',
+			viewer: 'text',
+			editor: 'textEditor',
+			rules: [v => !!v || 'Name is required'],
+			settings: {
+				// fieldWidth:70 //in %
+			},
+		},
+		{
+			id: 200,
+			text: 'Permissions',
+			value: 'permissions',
+			editor: 'autocomplete',
+			notViewable: true,
+			settings: {
+				limit: 100,
+				queryGql: permissionsGql,
+				itemValue: 'name',
+				itemText: 'name',
+				connectItem: true,
+				connectValue: 'permission', //required if connectItem
+				many: true,
+				model: 'permissions',
+				searchOptions: {
+					key: 'name',
+					op: '_ilike',
+				},
+				multiple: true,
+			},
+		},
+		{
+			id: 800,
+			text: 'Created At',
+			value: 'created_at',
+			viewer: 'date',
+			width: 180, //in px
+			notEditable: true,
+		},
+		{
+			id: 1000,
+			text: '#',
+			viewer: 'actions',
+			width: 50,
 			sortable: false,
 			notEditable: true,
 		},
